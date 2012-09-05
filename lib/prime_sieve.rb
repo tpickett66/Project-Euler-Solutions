17279ba5276bc6c8dd2244a6e847022e3e80997a
t.pickett66 <t.pickett66@gmail.com>
2012-09-05 10:50:33 -0500

    implement the Seive of Eratosthenes
    Signed-off-by: t.pickett66 <t.pickett66@gmail.com>

--- lib/prime_sieve.rb
+++ lib/prime_sieve.rb
@@ -1,48 +1,12 @@
 #= prime_seive.rb - My implementation of Sieve of Eratosthenes
+require 'set'
 
 module PrimeSeive
   def self.primes_less_than(n)
-    primes = []
-    
+    primes = (2..n).to_set
+    2.upto Math.sqrt(n) do |i|
+      primes.delete_if{|n| (n > i && n%i == 0)} if primes.include?(i)
+    end
+    primes.to_a
   end
 end
-
-# def sieve_upto(top)
-#   sieve = []
-#   for i in 2 .. top
-#     sieve[i] = i
-#   end
-#   for i in 2 .. Math.sqrt(top)
-#     next unless sieve[i]
-#     (i*i).step(top, i) do |j|
-#       sieve[j] = nil
-#     end
-#   end
-#   sieve.compact
-# end
-# I think it can be improved on slightly thus:
-# 
-# def better_sieve_upto(n)
-#   s = (0..n).to_a
-#   s[0] = s[1] = nil
-#   s.each do |p|
-#     next unless p
-#     break if p * p > n
-#     (p*p).step(n, p) { |m| s[m] = nil }
-#   end
-#   s.compact
-# end
-
-# while primes[index]**2 <= primes.last
-#       prime = primes[index]
-#       primes = primes.select { |x| x == prime || x%prime != 0 }
-#       index += 1
-# end
-# 
-# while Math.sqrt(primes.last).ceil >= primes[index]
-#     (primes[index] * 2).step(primes.last, primes[index]) do
-#       |x|
-#       primes.delete(x)
-#     end
-#     index += 1
-# end
--- spec/prime_sieve_spec.rb
+++ spec/prime_sieve_spec.rb
@@ -1,9 +1,12 @@
-require File.expand_path(File.dirname(__FILE__)) + '/test_helper'
+require File.expand_path(File.dirname(__FILE__)) + '/spec_helper'
 
 describe PrimeSeive do
   describe "finding primes up to a certain number" do
-    it "must work" do
-      1.must_equal 1
+    it "must return an array of primes less than the specified number" do
+      expected_primes = [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+        67, 71, 73, 79, 83, 89, 97]
+      PrimeSeive.primes_less_than(100).must_equal expected_primes
     end
   end
 end
