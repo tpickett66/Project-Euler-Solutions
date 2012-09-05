fd4d1376161baf25fb297ad37af1ae24a714292c
t.pickett66 <t.pickett66@gmail.com>
2012-09-05 10:53:11 -0500

    round that up so we make sure to get everything possible
    Signed-off-by: t.pickett66 <t.pickett66@gmail.com>

--- lib/prime_sieve.rb
+++ lib/prime_sieve.rb
@@ -4,7 +4,7 @@ require 'set'
 module PrimeSeive
   def self.primes_less_than(n)
     primes = (2..n).to_set
-    2.upto Math.sqrt(n) do |i|
+    2.upto Math.sqrt(n).ceil do |i|
       primes.delete_if{|n| (n > i && n%i == 0)} if primes.include?(i)
     end
     primes.to_a
