05750414e67ac4cc7d50c57d6f7528bc56d111e9
Tyler Pickett <t.pickett66@gmail.com>
2012-11-12 13:06:41 -0600

    require our new goodies
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'minitest/spec'
+require 'turn/autorun'
+require 'debugger'
+#require 'minitest/spec'
 
 ROOT_PATH = File.join(File.dirname(__FILE__), "..")
 
 $:.unshift(File.join(ROOT_PATH, 'lib'))
 
 require 'pes'
+
+Turn.config do |turn|
+  format = :progress
+end
